ea11c9055c9de65872768d319c731b5345cc1b5a
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-04-06 18:56:47 -0300

    Add Response and PagedResponse

---
+++ Fina.Core/Responses/PagedResponse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Responses
+{
+    internal class PagedResponse<TData> : Response<TData>
+    {
+        [JsonConstructor]
+        public PagedResponse(
+            TData? data,
+            int totalCount,
+            int currentPage = 1,
+            uint pageSize = Configuration.DefaultPageSize
+        ) : base(data)
+        {
+            Data = data;
+            TotalCount = totalCount;
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+        }
+
+        public PagedResponse
+        (
+            TData? data,
+            int code = Configuration.DefaultStatusCode,
+            string? message = null
+        ) : base(data, code, message)
+        { }
+        public int CurrentPage { get; set; }
+        public int TotalPages => (int) Math.Ceiling(
+            TotalCount / (double) PageSize);
+        public uint PageSize { get; set; } = Configuration.DefaultPageSize;
+        public int TotalCount { get; set; }
+
+
+    }
+}
--- Fina.Core/Responses/Response.cs
+++ Fina.Core/Responses/Response.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Fina.Core.Responses
 {
-    internal class Response
+    internal class Response<TData>
     {
+
+        [ JsonConstructor ]
+        public Response()
+            =>  _code = Configuration.DefaultStatusCode;
+        
+        public Response(
+            TData? data,
+            int code = Configuration.DefaultStatusCode,
+            string? message = null
+        )
+        {
+            Data = data;
+            _code = code;
+            Message = message;
+        }
+
+        private readonly int _code = Configuration.DefaultStatusCode; // Http status code -> 200 = 
+        public TData? Data { get; set; }
+        public string? Message { get; set; }
+
+        [ JsonIgnore ]
+        public bool IsSuccess => _code is >= 200 and <= 299;
     }
 }
