015d8cb674c77df64579b272d7dc055999429a39
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-04-06 18:57:28 -0300

    Add Transaction Requests

---
+++ Fina.Core/Requests/Transactions/GetTransactionsByPeriodRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Transactions
+{
+    internal class GetTransactionsByPeriodRequest : PagedRequest
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
---
+++ Fina.Core/Requests/Transactions/UpdateTransactionRequest.cs
@@ -0,0 +1,30 @@
+﻿using Fina.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Transactions
+{
+    internal class UpdateTransactionRequest : Request
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "Invalid title")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Invalid type")]
+        public ETransationType Type { get; set; } = ETransationType.Withdraw;
+
+        [Required(ErrorMessage = "Invalid amount")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Invalid category")]
+        public long CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Invalid date")]
+        public DateTime? PaidOrReceivedAt { get; set; }
+    }
+}
