90737f871b3182a7780de2ce26576d36b1ef18f4
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-04-06 18:57:51 -0300

    Add DefaultStatusCode = 200

--- Fina.Core/Configuration.cs
+++ Fina.Core/Configuration.cs
@@ -8,6 +8,7 @@ namespace Fina.Core
 {
     internal static class Configuration
     {
+        public const int DefaultStatusCode = 200;
         public const uint DefaultPageNumber = 1;
         public const uint DefaultPageSize = 25;
     }
