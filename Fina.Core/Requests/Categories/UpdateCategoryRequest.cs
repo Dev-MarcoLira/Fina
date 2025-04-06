49091d412f052dc784b3ff6781fb7b0a3b12d8a6
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-04-06 00:08:30 -0300

    Add Requests / Categories

---
+++ Fina.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests.Categories
+{
+    internal class UpdateCategoryRequest : Request
+    {
+        [Required(ErrorMessage = "Invalid Title")]
+        [MaxLength(80, ErrorMessage = "The title must contain up to 80 characters")]
+        public String Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Invalid Description")]
+        public String Description { get; set; } = string.Empty;
+    }
+}
---
+++ Fina.Core/Requests/PagedRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Requests
+{
+    internal abstract class PagedRequest : Request
+    {
+        public uint PageSize { get; set; } = Configuration.DefaultPageSize;
+        public uint PageNumber { get; set; } = Configuration.DefaultPageNumber;
+    }
+}
