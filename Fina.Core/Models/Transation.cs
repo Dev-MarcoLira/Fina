f8e23a580cfb7d3e53d0557b38a6e0742d554ad8
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-04-05 23:23:02 -0300

    Add Transaction and Category

---
+++ Fina.Core/Models/Transation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fina.Core.Models
+{   
+    // Financial Release
+    internal class Transation
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdatedAt { get; set;} = DateTime.Now;
+        public DateTime? PaidOrReceivedAt { get; set; }
+
+        // Input or Output
+        public bool Type { get; set; }
+
+        public long CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
